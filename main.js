cf548d7ffc00119a11b6307e2e55805b0e630e66
Shlomi <vgtvgy1@gmail.com>
2023-09-10 13:18:40 +0300

    Working merge sort

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const hovered_bar_div = document.getElementById("bar-value-div");
 const hovered_bar_span = document.getElementById("bar-value");
 const btn_stop_sort = document.getElementById("btn-stop-sort");
 const btn_insertion_sort = document.getElementById("btn-insertion-sort");
+const btn_merge_sort = document.getElementById("btn-merge-sort");
 
 let is_sorting = false;
 
@@ -26,11 +27,11 @@ function onload() {
     var hoveredBarIndex = -1;
 
     canvas.addEventListener("mouseenter", () => {
-        if(is_sorting)
+        if (is_sorting)
             return;
         hovered_bar_div.style.opacity = "1";
     });
-    
+
     canvas.addEventListener("mousemove", (event) => {
         if (is_sorting)
             return;
@@ -70,7 +71,7 @@ function onload() {
     btn_randomize.addEventListener("click", () => {
         randomize();
     });
-    
+
     btn_bubble_sort.addEventListener("click", () => {
         bubbleSort();
     });
@@ -78,7 +79,11 @@ function onload() {
     btn_insertion_sort.addEventListener("click", () => {
         insertion_sort();
     });
-    
+
+    btn_merge_sort.addEventListener("click", () => {
+        merge_sort();
+    });
+
     // Add value change listener to slider
     document.getElementById("num-bars-range").addEventListener("input", (event) => {
         uniqueHeights.length = event.target.value;
@@ -140,8 +145,8 @@ async function bubbleSort() {
     sort_start();
 
     n = uniqueHeights.length;
-    for(let i = 0; i < n; i++) {
-        for(let j = 0; j < n - i - 1; j++) {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
             if (!is_sorting) {
                 draw();
                 return;
@@ -158,7 +163,7 @@ async function bubbleSort() {
             const delay = document.getElementById("sort-delay-range").value;
             await new Promise(r => setTimeout(r, delay));
 
-            if(a > b) {
+            if (a > b) {
                 [uniqueHeights[j], uniqueHeights[j + 1]] = [b, a];
             }
             draw();
@@ -173,7 +178,7 @@ function sort_start() {
 }
 
 async function sort_end() {
-    for(let i = 0; i < uniqueHeights.length; i++) {
+    for (let i = 0; i < uniqueHeights.length; i++) {
         const delay = document.getElementById("sort-delay-range").value;
         await new Promise(r => setTimeout(r, delay));
 
@@ -193,6 +198,7 @@ function disable_elements() {
     btn_stop_sort.disabled = false;
     btn_insertion_sort.disabled = true;
     hovered_bar_div.style.opacity = "0";
+    btn_merge_sort.disabled = true;
 }
 
 function enable_elements() {
@@ -202,6 +208,7 @@ function enable_elements() {
     range_num_bars.disabled = false;
     btn_stop_sort.disabled = true;
     btn_insertion_sort.disabled = false;
+    btn_merge_sort.disabled = false;
 }
 
 async function insertion_sort() {
@@ -222,7 +229,7 @@ async function insertion_sort() {
 
             // Change color of the bars being compared
             draw_bar(j, "blue");
-            draw_bar(j+1, "blue");
+            draw_bar(j + 1, "blue");
             draw_bar(i, "red");
 
             // Apply delay
@@ -234,7 +241,7 @@ async function insertion_sort() {
 
             draw();
         }
-        draw_bar(j+1, "green");
+        draw_bar(j + 1, "green");
         const delay = document.getElementById("sort-delay-range").value;
         await new Promise(r => setTimeout(r, delay));
         uniqueHeights[j + 1] = key;
@@ -244,7 +251,139 @@ async function insertion_sort() {
     sort_end();
 }
 
+async function merge_sort() {
+    if (is_sorting)
+        return;
+
+    sort_start();
+
+    async function merge(arr, l, m, r) {
+        const n1 = m - l + 1;
+        const n2 = r - m;
+
+        const L = new Array(n1);
+        const R = new Array(n2);
+
+        for (let i = 0; i < n1; i++) {
+            L[i] = arr[l + i];
+        }
+        for (let j = 0; j < n2; j++) {
+            R[j] = arr[m + 1 + j];
+        }
+
+        let i = 0;
+        let j = 0;
+        let k = l;
+
+        while (i < n1 && j < n2) {
+            if (!is_sorting) {
+                draw();
+                return;
+            }
+
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+
+            draw_bar(k, "blue");
+            const delay = document.getElementById("sort-delay-range").value;
+            await new Promise(r => setTimeout(r, delay));
+
+            draw();
+            k++;
+        }
+
+        while (i < n1) {
+            if (!is_sorting) {
+                draw();
+                return;
+            }
+
+            arr[k] = L[i];
+            draw_bar(k, "blue");
+
+            // Apply delay
+            const delay = document.getElementById("sort-delay-range").value;
+            await new Promise(r => setTimeout(r, delay));
+
+            draw();
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            if (!is_sorting) {
+                draw();
+                return;
+            }
+
+            arr[k] = R[j];
+            draw_bar(k, "blue");
+
+            // Apply delay
+            const delay = document.getElementById("sort-delay-range").value;
+            await new Promise(r => setTimeout(r, delay));
+
+            draw();
+            j++;
+            k++;
+        }
+    }
+
+    async function mergeSortHelper(arr, l, r) {
+        if (l < r) {
+            const m = Math.floor((l + r) / 2);
+
+            await mergeSortHelper(arr, l, m);
+            await mergeSortHelper(arr, m + 1, r);
+
+            await merge(arr, l, m, r);
+        }
+    }
+
+    await mergeSortHelper(uniqueHeights, 0, uniqueHeights.length - 1);
+
+    sort_end();
+}
+
 document.getElementById("debug").addEventListener("click", () => {
+    // Create an AudioContext
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+    // Function to generate a random 8-bit sound for 10ms
+    function playRandom8BitSound() {
+        const oscillator = audioContext.createOscillator();
+        const gainNode = audioContext.createGain();
+
+        oscillator.type = 'square'; // 8-bit-like square wave
+        oscillator.frequency.setValueAtTime(
+            Math.random() * 2000 + 500, // Random frequency between 500 and 2500 Hz
+            audioContext.currentTime
+        );
+
+        gainNode.gain.setValueAtTime(0.5, audioContext.currentTime); // Set the volume
+
+        oscillator.connect(gainNode);
+        gainNode.connect(audioContext.destination);
+
+        oscillator.start();
+        oscillator.stop(audioContext.currentTime + 0.01); // Play for 10ms
+    }
+
+    // Function to play a burst of random 8-bit sounds
+    function playBurstOfSounds(burstLength) {
+        for (let i = 0; i < burstLength; i++) {
+            setTimeout(playRandom8BitSound, i * 15); // Play a sound every 15ms
+        }
+    }
+
+    // Call the function to play a burst of 8-bit sounds (adjust the burst length as needed)
+    playBurstOfSounds(10); // Play 10 random sounds
+
 });
 
 window.addEventListener("load", onload);
